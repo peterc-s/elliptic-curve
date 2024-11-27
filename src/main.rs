bd4a123336178aa1e2b29ebc0146f3756522c2cc
peterc-s <petercs@skiff.com>
2024-11-27 21:50:01 +0000

    point multiplication

--- src/main.rs
+++ src/main.rs
@@ -47,20 +47,20 @@ impl Point {
 
     fn add_curve(&self, other: &Point, p: &ModMath, curve: &EllipticCurve) -> Self {
         if *self == *other || self.x == other.x {
-            return self.double(curve);
+            return self.double(p, curve);
         }
         if *self == Self::identity() {
-            return self.clone();
+            return other.clone();
         }
         if *other == Self::identity() {
-            return other.clone();
+            return self.clone();
         }
 
         let lambda = {
             // y2 - y1 / x2 - x1
             let numerator = p.sub(other.y, self.y);
             let denominator = p.sub(other.x, self.x);
-            p.mul(numerator, p.inv(denominator).unwrap())
+            p.div(numerator, denominator)
         };
 
         // x3 = \lambda^2 - x2 - x1
@@ -72,8 +72,43 @@ impl Point {
         Point { x, y }
     }
 
-    fn double(&self, curve: &EllipticCurve) -> Self {
-        todo!()
+    fn double(&self, p: &ModMath, curve: &EllipticCurve) -> Self {
+        if *self == Self::identity() {
+            return self.clone();
+        }
+
+        let lambda = {
+            // 3x^2
+            let numerator = p.mul(U256::from(3), p.exp(self.x, U256::from(2)));
+            // 2y
+            let denominator = p.mul(U256::from(2), self.y);
+            // (3x^2 + a) / 2y
+            p.div(p.add(numerator, U256::from(curve.a)), denominator)
+        };
+
+        // x' = \lambda^2 - 2x
+        let x: U256 = p.sub(p.exp(lambda, U256::from(2)), p.mul(U256::from(2), self.x));
+        // y' = \lambda(x - x') - y
+        let y: U256 = p.sub(p.mul(lambda, p.sub(self.x, x)), self.y);
+
+        Point { x, y }
+    }
+
+    fn mult(&self, k: &U256, p: &ModMath, curve: &EllipticCurve) -> Self {
+        let mut result = Self::identity();
+        let mut current = self.clone();
+
+        let mut k = k.clone();
+
+        while k > U256::zero() {
+            if k % U256::from(2) == U256::one() {
+                result = result.add_curve(&current, p, curve);
+            }
+            current = current.double(p, curve);
+            k = k >> 1;
+        }
+
+        result
     }
 }
 
@@ -98,12 +133,14 @@ fn main() -> Result<()> {
     let curve_point_1 = Point::new_from_curve(U256::from(17u8), &mod_p, &test_curve);
     let curve_point_2 = Point::new_from_curve(U256::from(95u8), &mod_p, &test_curve);
 
-    println!("Curve point 1: {}, {}", curve_point_1.x, curve_point_1.y);
-    println!("Curve point 2: {}, {}", curve_point_2.x, curve_point_2.y);
+    println!("Curve point 1: {:?}", curve_point_1);
+    println!("Curve point 2: {:?}", curve_point_2);
 
     let curve_points_added = curve_point_1.add_curve(&curve_point_2, &mod_p, &test_curve);
 
-    println!("Curve points added: {}, {}", curve_points_added.x, curve_points_added.y);
+    println!("Curve points added: {:?}", curve_points_added);
+    println!("First point doubled: {:?}", curve_point_1.double(&mod_p, &test_curve));
+    println!("First point 7P: {:?}", curve_point_1.mult(&U256::from(7), &mod_p, &test_curve));
 
     Ok(())
 }
